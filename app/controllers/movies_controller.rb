f482cc84fcb369a075743a2d1c2c4809a5c529b0
S Hamsaraj Hamsaraj <1005943@mymail.sutd.edu.sg>
2023-06-11 15:18:08 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show_hash = params[:ratings] || {}
+    @movies = Movie.with_ratings(@ratings_to_show_hash.keys)
   end
 
   def new
